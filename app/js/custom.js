28f612f610615cc5d282b52e2c5014d4c316d3f3
lunberg88 <lunberg88@gmail.com>
2017-12-11 19:45:15 +0200

    New pages: contacts, blog, expertise_one

---
+++ app/js/custom.js
@@ -0,0 +1,44 @@
+$(function() {
+    function init() {
+        window.addEventListener('scroll', function(e){
+            var distanceY = window.pageYOffset || document.documentElement.scrollTop,
+                shrinkOn = 300,
+                header = document.querySelector("header");
+            if (distanceY > shrinkOn) {
+                classie.add(header,"smaller");
+            } else {
+                if (classie.has(header,"smaller")) {
+                    classie.remove(header,"smaller");
+                }
+            }
+        });
+    }
+    window.onload = init();
+
+    $('.carousel').carousel({
+        interval: 5000*1000
+    });
+
+    $(".filter-button").click(function(){
+        var value = $(this).attr('data-filter');
+        var clbtn = $(this);
+        $('.actives').removeClass('actives');
+        clbtn.addClass('actives');
+
+        if(value == "all")
+        {
+            $('.filter').show('1000');
+        }
+        else
+        {
+            $(".filter").not('.'+value).hide('3000');
+            $('.filter').filter('.'+value).show('3000');
+
+        }
+    });
+
+    if ($(".filter-button").removeClass("active")) {
+        $(this).removeClass("active");
+    }
+    $(this).addClass("active");
+});
